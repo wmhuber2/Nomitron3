b9bca2951e7c5075ed7e35f0671b19b3bc58cd09
wolfgang <wmhuber2@gmail.com>
2022-10-12 22:59:13 -0500

    Nomitron Update to N6

--- data/VotingRules.py
+++ data/VotingRules.py
@@ -21,7 +21,7 @@ async def removeSupporter(Data, payload, *text):
 
 async def extendTurn(Data, payload, *text):
     Data['NextTurnStartTime'] += 24*60*60
-    await payload['raw'].channel.send('Turn extended 24 hrs. Use !tickTurn to manually trigger the next turn if needed')
+    payload['raw'].channel.send('Turn extended 24 hrs. Use !tickTurn to manually trigger the next turn if needed')
     return Data
 
 async def removeProposal(Data, payload, *text):
