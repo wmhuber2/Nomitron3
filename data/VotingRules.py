263afb769349148187a94c6eedfcce4db5166262
wolfgang <wmhuber2@gmail.com>
2022-12-06 19:41:19 -0600

    fix turnStats

--- data/VotingRules.py
+++ data/VotingRules.py
@@ -23,11 +23,11 @@ def now(): return time.time() - zeroday
 def getTime(t):
     day =  t// (24*3600)
 
-    sc  = (t % (60  )    ) // (1      )
-    mn  = (t % (60*60)   ) // (60     )
-    hr  = (t % (24*60*60)) // (60*60  )
-    day = (t             ) // (24*3600)
-    return f"{day}-{hr}:{mn} {sc}s"
+    sc  = int((t % (60  )    ) // (1      ))
+    mn  = int((t % (60*60)   ) // (60     ))
+    hr  = int((t % (24*60*60)) // (60*60  ))
+    day = int((t             ) // (24*3600))
+    return f"{day} @ {hr}:{mn} {sc}s"
 
 async def turnStats(Data, payload, *text):
     msg = f'''**Turn Stats:**```
